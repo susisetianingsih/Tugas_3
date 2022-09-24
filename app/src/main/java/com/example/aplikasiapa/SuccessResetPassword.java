cc2d73c511bf776a91a51b25ea316cd91a6dc788
susisetianingsih <susisetia542@gmail.com>
2022-09-24 22:07:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasiapa/SuccessResetPassword.java
@@ -0,0 +1,14 @@
+package com.example.aplikasiapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_reset_password);
+    }
+}
---
+++ app/src/main/java/com/example/aplikasiapa/WelcomeSlide.java
@@ -0,0 +1,14 @@
+package com.example.aplikasiapa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeSlide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide);
+    }
+}
